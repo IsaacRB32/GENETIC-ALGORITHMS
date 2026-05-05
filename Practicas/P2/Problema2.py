5141fdc5dee1bd686a4ff336b61b8dc17d9f8950
IsaacRB32 <148009185+IsaacRB32@users.noreply.github.com>
2026-05-05 13:16:46 -0600

    Práctica 2, Problema2.

--- Practicas/P2/Problema2.py
+++ Practicas/P2/Problema2.py
@@ -2,7 +2,7 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
-import pandas as pd # Para la tabla final, es más limpio
+import pandas as pd
 
 def genPoblacionReal(Np, Num_var, li, ls):
 
